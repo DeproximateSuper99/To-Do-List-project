b06a104d0d9bb033d0711820eb7c7cb1b346bed1
DeproximateSuper99 <aryanghag455@gmail.com>
2025-04-18 18:59:16 +0530

    Create manifest.js
    for android support

---
+++ manifest.js
@@ -0,0 +1,21 @@
+{
+    "name": "Earth Focus",
+    "short_name": "Focus",
+    "start_url": ".",
+    "display": "standalone",
+    "background_color": "#121212",
+    "theme_color": "#ffb6c1",
+    "icons": [
+      {
+        "src": "icon-192.png",
+        "type": "image/png",
+        "sizes": "192x192"
+      },
+      {
+        "src": "icon-512.png",
+        "type": "image/png",
+        "sizes": "512x512"
+      }
+    ]
+  }
+  
